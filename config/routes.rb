2da50326a45b88f9b5629b02e0142a5a5f50589a
albertmjimenez <albertmjimenez09@gmail.com>
2017-01-17 12:50:45 -0600

    Updated Space WLVS Merch to Tour

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about' => 'pages#about'
   get 'contact' => 'pages#contact'
-  get 'merch' => 'pages#merch'
+  get 'tour' => 'pages#tour'
+  get 'pictures' => 'pages#pictures'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
